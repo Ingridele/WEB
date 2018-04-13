fd6ff9c6418a0b2fba7f5a79777458f5d266ae25
Ingridelei <ingrida.narsutyte@mail.com>
2018-04-13 11:26:38 +0300

    pridejau 5,6,7 ir 9 js namu darbus, 8 nesupratau

--- Namu darbai/js/js3/js/script.js
+++ Namu darbai/js/js3/js/script.js
@@ -1,11 +1,11 @@
 
-var amzius = prompt("Kiek jums metų?" , "0");
+var amzius = prompt("Kiek jums metų?", "0");
 var atsakymas = amziausCenzas(amzius);
-function amziausCenzas (amzius){
+function amziausCenzas(amzius){
   if (amzius >=18){
-  alert("Galite užeiti į svetainę");
+    alert("Galite užeiti į svetainę");
   }
   else{
-  alert("Į svetainę užeiti negalite");
+    alert("Į svetainę užeiti negalite");
   }
 }
